a98a7755656a822698a8d38a44760da9e19b6722
Vera <Vera@192.168.1.15>
2021-12-14 11:52:35 +0200

    loginTest 2 tests

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -44,7 +44,7 @@ public class LoginPageTest extends TestBase{
 	}
 	
 	
-	@Test(priority=3)
+	@Test(priority=3, enabled=false )
 	public void loginTest() {
 		
 		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password")); 
